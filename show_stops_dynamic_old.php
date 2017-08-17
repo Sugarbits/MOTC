33825ac1e1fc6f5a44a087b56b3463d07877eab4
Sugarbits <b1030536039@gmail.com>
2017-08-17 22:17:32 +0800

    buck
    gmap & bus stop list

--- show_stops_dynamic_old.php
+++ show_stops_dynamic_old.php
@@ -17,33 +17,12 @@ echo $jsword;
 	td{
 		font-size:150%;
 	}
-	#map{
-		z-index: 1;
-		top:0%;
-		//position:absolute;
-		float:right;
-		width:70%;
-		height:100vh;
-		background-color:black;
-	}
-	#main{
-		z-index: 1;
-		top:0%;
-		//position:absolute;
-		float:right;
-		width:30%;
-		height:100vh;
-	}
-	tr:nth-child(even) {background: #CCC}
-	tr:nth-child(odd) {background: #FFF}
 	</style>
   </head>
   <body>
   <h1 id='routecode'></h1>
   <!--<h1 id='routename'></h1>-->
   <div id='main'></div>
-  <!--地圖主體-->
-  <div id="map"></div>
   <BR>
   <div>上次更新時間：<span id='remain'></span><span>秒</span></div>
    <!--地圖主體-->
@@ -54,9 +33,6 @@ echo $jsword;
 	   UpdateTime:'',
 	   Timer:''
    };
-   var markers =[];
-   var map;
-   var now_icon_url = 'pic/busicon.png';
 	function wrap_td(str){
 		return '<td>'+str+'</td>';
 	}
@@ -69,32 +45,12 @@ echo $jsword;
 	function initial(){//ajax once data
 	var RouteName = '';
 		$.getJSON( "crawler/motc_bus_dynamic.php?route="+_route+"&direct="+_direct+"&citycode="+_citycode+"&func=0", function( data ) {
-			//console.log(data);
+			console.log(data);
 			for(key in data){
-				if(key == _direct){
-					RouteName = data[key]['RouteName']['Zh_tw'];
-					//console.log(data[key]['Stops']);
-					for(key2 in data[key]['Stops']){
-						//console.log('initial');
-						//console.log(data[key]['Stops'][key2]);
-						var lat = data[key]['Stops'][key2]['StopPosition']['PositionLat'];
-						var lon = data[key]['Stops'][key2]['StopPosition']['PositionLon'];
-						var title = {'name':data[key]['Stops'][key2]['StopName']['Zh_tw']};
-						LatLng = {lat : lat,lng :　lon};//google map latlng obj 
-						var sa = add_marker(map,LatLng,title);
-						markers.push(sa);
-						console.log(markers);
-						//console.log('initial');
-						/*if(key2 == 'Stops'){
-							console.log(data[key][key2]['StopPosition']);
-						}*/
-					}
+				RouteName = data[key]['RouteName']['Zh_tw'];
 				}
-			}
 				$('#routecode').html(RouteName);
-				
-			}).done(setInterval(function(){ panto_muti_marker(markers); }, 1000));
-			
+			});
 			
 	}
 	function touch(){//ajax 試探值的變化
@@ -161,63 +117,14 @@ echo $jsword;
 			});
 	}
 	////
-	function google_map_initial(){
-		var myLatLng = {lat: 23.7, lng: 121.4};
-		map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 15,
-			center: myLatLng
-        });
-		// Create the DIV to hold the control and call the CenterControl()
-        // constructor passing in this DIV.
-		//renew();
-      }
-	function add_marker(a_map,a_latlng,a_title){
-			var marker = new google.maps.Marker({
-				position: a_latlng,
-				map: a_map,
-				title : a_title['name'] /*+ "\n" + a_title['time']*/
-				//icon : (now_icon_url)
-			});
-			 
-			//map.panTo(tmpLatLng);
-			//bindInfoWindow(marker, map, infowindow, '<b>'+places[p].name + "</b><br>" + places[p].geo_name);
-			// not currently used but good to keep track of markers
-			//markers[data[key]['PlateNumb']].push(marker);
-			//http://maps.google.com/mapfiles/ms/icons/blue-dot.png
-			//console.log(markers);
-			return marker;
-	}
-	function panto_muti_marker(pmarkers){
-		console.log('panto_muti_marker');
-		console.log(pmarkers);
-		console.log('panto_muti_marker');
-		if(pmarkers.length != 0){
-			var bounds = new google.maps.LatLngBounds();
-			for (var i = 0; i < pmarkers.length; i++) {
-				var point = new google.maps.LatLng(
-				parseFloat(pmarkers[i].getPosition().lat()),
-				parseFloat(pmarkers[i].getPosition().lng()));
-				// add each marker's location to the bounds
-				bounds.extend(point);
-			}
-		map.fitBounds(bounds);
-		}else{
-			alert('沒有公車！');
-		}
-	}
 	  $(function(){
-		 $(window).on('load',function(){
-			google_map_initial();
-			initial();
-			touch();
-			setInterval(function(){ touch(); }, 3000);
-		});
-		
+		 initial();
+		 touch();
+		 setInterval(function(){ touch(); }, 3000);
 		
 	});
 	
 	  
     </script>
-	 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA0bdKmBEMTJH7qsTjjG_1rfteVrNXzxQk"></script>
   </body>
 </html>
